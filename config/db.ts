5b89376965b1640ddfa1167667948627e139e42c
David <eth3rnit3@outlook.fr>
2020-05-29 23:27:54 +0200

    fix db uri

--- config/db.ts
+++ config/db.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
+const dbUri = Deno.env.toObject().MONGODB_URI || 'mongodb://localhost:27017';
 
 const client = new MongoClient();
-client.connectWithUri("mongodb://localhost:27017");
+client.connectWithUri(dbUri);
 
 export const db = client.database("deno_chat");
